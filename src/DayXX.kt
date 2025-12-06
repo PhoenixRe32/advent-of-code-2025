b2a4b3d8f4f34c908b2e38f16d8d59738a1d4419
Andrea Andreou <andreas.andreou@masabi.com>
2025-12-06 17:18:54 +0200

    add template for copy paste

---
+++ src/DayXX.kt
@@ -0,0 +1,18 @@
+class DayXX : Day<Long> {
+    override val day = "DayXX"
+    override fun part1(input: List<String>): Long {
+        return 0
+    }
+
+    override fun part2(input: List<String>): Long {
+        return 0
+    }
+
+}
+
+fun main() {
+    DayXX().run {
+        test(0, 0)
+        execute()
+    }
+}
