8143411d1c88b68c06b6bf9ee1f956c729f4da1f
Andrea Andreou <andreas.andreou@masabi.com>
2025-12-06 15:11:30 +0200

    Day interface for common structure for my convenience

---
+++ src/Day.kt
@@ -0,0 +1,22 @@
+interface Day<N : Number> {
+    fun part1(input: List<String>): N
+    fun part2(input: List<String>): N
+    val day: String
+
+    fun test(part1ExpectedValue: N, part2ExpectedValue: N) {
+        val testInput = readInput("${day}_test")
+        val part1Test = part1(testInput)
+        check(part1Test == part1ExpectedValue) { "Example test (1) failed: $part1Test" }
+        val part2Test = part2(testInput)
+        check(part2Test == part2ExpectedValue) { "Example test (2) failed: $part2Test" }
+    }
+
+    fun execute() {
+        val input = readInput(day)
+        println("PART 1")
+        part1(input).println()
+        println("=====")
+        println("PART 2")
+        part2(input).println()
+    }
+}
