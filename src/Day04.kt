c59f8bcd3dc608b445ff1f18fd2f812851bf2ff2
Andrea Andreou <andreas.andreou@masabi.com>
2025-12-07 12:06:33 +0200

    refactor: Use chars

--- src/Day04.kt
+++ src/Day04.kt
@@ -13,8 +13,8 @@ class Day04 : Day<Int> {
         val map = createAndPopulateMap(input)
         return map.indices.sumOf { row ->
             map[row].indices.sumOf { col ->
-                if (!map[row][col]) return@sumOf 0
-                val numberOfRolls = map.countEnclosingSquareOf(row, col) { it }
+                if (map[row][col]=='.') return@sumOf 0
+                val numberOfRolls = map.countEnclosingSquareOf(row, col) { it==ROLL_OF_PAPER }
                 // we are counting the square itself, so we add 1 to the rule of the quiz
                 if (numberOfRolls < 5) 1 else 0
             }
@@ -26,24 +26,24 @@ class Day04 : Day<Int> {
     }
 }
 
-fun Array<Array<Boolean>>.countEnclosingSquareOf(row: Int, col: Int, predicate: (Boolean) -> Boolean): Int =
+fun Array<Array<Char>>.countEnclosingSquareOf(row: Int, col: Int, predicate: (Char) -> Boolean): Int =
     rangeOfNeighbouringRows(row).sumOf { row ->
         this[row].rangeOfNeighbouringColumns(col).count { col -> predicate(this[row][col]) }
     }
 
-private fun Array<Array<Boolean>>.rangeOfNeighbouringRows(row: Int): IntRange {
+private fun Array<Array<Char>>.rangeOfNeighbouringRows(row: Int): IntRange {
     return (row - 1).coerceAtLeast(0)..(row + 1).coerceAtMost(lastIndex)
 }
 
-private fun Array<Boolean>.rangeOfNeighbouringColumns(col: Int): IntRange {
+private fun Array<Char>.rangeOfNeighbouringColumns(col: Int): IntRange {
     return (col - 1).coerceAtLeast(0)..(col + 1).coerceAtMost(lastIndex)
 }
 
-fun createAndPopulateMap(input: List<String>): Array<Array<Boolean>> {
+fun createAndPopulateMap(input: List<String>): Array<Array<Char>> {
     return createMap(input).apply {
         input.forEachIndexed { row, line ->
             line.toCharArray().forEachIndexed { col, char ->
-                this[row][col] = char == ROLL_OF_PAPER
+                if (char == ROLL_OF_PAPER) this[row][col] = ROLL_OF_PAPER
             }
         }
     }
@@ -52,5 +52,5 @@ fun createAndPopulateMap(input: List<String>): Array<Array<Boolean>> {
 // My map ordering is rows and columns
 // E.g. [3][5] means 3rd row, 5th column
 // Outer index is rows, innner is columns
-fun createMap(input: List<String>): Array<Array<Boolean>> =
-    Array(input.size) { Array(input[0].length) { false } }
+fun createMap(input: List<String>): Array<Array<Char>> =
+    Array(input.size) { Array(input[0].length) { '.' } }
