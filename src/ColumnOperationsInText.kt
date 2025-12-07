1405cd0c29a8522ab725ac5073116b6cde2b30d9
Andrea Andreou <andreas.andreou@masabi.com>
2025-12-07 19:49:09 +0200

    Day 06 Part 2 preparation
    Maintain spaces in strings for operands

--- src/ColumnOperationsInText.kt
+++ src/ColumnOperationsInText.kt
@@ -4,40 +4,57 @@
 object ColumnOperationsInText {
 
     fun operateOnColumn(input: List<String>): Pair<Long, List<String>> {
-        val inputForNext = mutableListOf<String>()
+        val inputForNext = MutableList(input.size) { "" }
 
         val (operator, remainingOperationsLine) = input.last().extractFirstWordAndReturnRemainder()
-        inputForNext.add(0, remainingOperationsLine)
+        inputForNext[input.size - 1] = remainingOperationsLine
 
         val op = OP.from(operator)
 
         val resultOfColumn = (0 until input.size - 1).fold(op.identity) { acc, i ->
             val (operand, remainingOperandsLines) = input[i].extractFirstWordAndReturnRemainder()
-            inputForNext.add(i, remainingOperandsLines)
-            op.fn(acc, operand.toLong())
+            inputForNext[i] = remainingOperandsLines
+            op.fn(acc, operand.trim().toLong())
         }
 
         return resultOfColumn to inputForNext
     }
 
-    private fun String.extractFirstWordAndReturnRemainder() =
-        trim().run {
-            takeWhile { char -> !char.isWhitespace() }
-                .let { word -> word to substring(word.length).trim() }
+    private fun String.extractFirstWordAndReturnRemainder(): Pair<String, String> {
+        var nextSpaceIsStop = false
+        val word = StringBuilder()
+        for (c in toCharArray()) {
+            if (c.isWhitespace() && nextSpaceIsStop) break
+            word.append(c)
+            nextSpaceIsStop = !c.isWhitespace()
         }
 
-
+        return word.toString() to substring(word.length)
+    }
 }
+
 
 enum class OP(val identity: Long, val fn: (x: Long, y: Long) -> Long) {
     SUM(0, { a, b -> a + b }),
     MUL(1, { a, b -> a * b });
 
     companion object {
-        fun from(operator: String) = when (operator) {
+        fun from(operator: String) = when (operator.trim()) {
             "+" -> SUM
             "*" -> MUL
             else -> throw IllegalArgumentException("Unknown operator: $operator")
         }
     }
+}
+
+fun power(baseVal: Int, exponentVal: Int): Long {
+    val base = baseVal
+    var exponent = exponentVal
+    var result: Long = 1
+
+    while (exponent != 0) {
+        result *= base.toLong()
+        --exponent
+    }
+    return result
 }
